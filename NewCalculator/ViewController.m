f0262ceeac01437ef46fa287ac3df369586e6bfb
Olof Hellquist <olofhellquist@gmail.com>
2013-07-04 22:22:53 +0200

    Added highlighting of operator buttons and a minus button.

--- NewCalculator/ViewController.m
+++ NewCalculator/ViewController.m
@@ -14,6 +14,7 @@
 @property double currentValue;
 @property BOOL newEntry;
 @property (strong, nonatomic) MathOperator *operator;
+@property (strong, nonatomic) NSMutableArray *operatorButtons;
 
 @end
 
@@ -23,11 +24,26 @@
 @synthesize newEntry = _newEntry;
 @synthesize operator = _operator;
 @synthesize display = _display;
+@synthesize operatorButton = _operatorButton;
+@synthesize minusButton = _minusButton;
+@synthesize operatorButtons = _operatorButtons;
+
+- (NSMutableArray *)operatorButtons
+{
+    if (_operatorButtons == NULL) {
+        _operatorButtons = [[NSMutableArray alloc] initWithCapacity:2];
+        [_operatorButtons addObject:self.operatorButton];
+        [_operatorButtons addObject:self.minusButton];
+    }
+    return _operatorButtons;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     self.newEntry = YES;
+
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(disableOperator:) name:@"DigitPressed" object:nil];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -66,18 +82,44 @@
     return [self.display.text doubleValue];
 }
 
+
+- (void)disableOperator:(NSNotification *)notification
+{
+    //[operator setHighlighted:NO];
+    for (int i = 0; i < [self.operatorButtons count]; i++) {
+        UIButton *button = [self.operatorButtons objectAtIndex:i];
+        
+        [self performSelector:@selector(turnOffHighlightButton:) withObject:button afterDelay:0.0];
+    }
+}
+
 - (IBAction)digitPressed:(UIButton *)sender
 {
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"DigitPressed" object:nil];
+    
     NSString *digit = [sender currentTitle];
     [self addDigit:digit];
 }
 
+- (void)highlightButton:(UIButton *)button
+{
+    [button setHighlighted:YES];
+}
+
+- (void)turnOffHighlightButton:(UIButton *)button
+{
+    [button setHighlighted:NO];
+}
+
 - (IBAction)operatorPressed:(UIButton *)sender
 {
-    BOOL performCalculation = [self hasOperator];
+    [self disableOperator:nil];
+    [self performSelector:@selector(highlightButton:) withObject:sender afterDelay:0.0];
+    
+    MathOperator *oldOperator = self.operator;
     self.operator = [MathOperator createWithString:[sender currentTitle]];
     
-    if (performCalculation) {
+    if ([oldOperator class] == [self.operator class]) {
         double result = [self performOperation:self.currentValue :[self getDisplayValue]];
         [self displayResult:result];
     }
