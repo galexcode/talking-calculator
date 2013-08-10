90db39d8c0e1b6074232ddf4e285cc76d019cad1
Olof Hellquist <olofhellquist@gmail.com>
2013-08-10 14:24:13 +0200

    Refactored leopard button.

--- NewCalculator/LeopardButton.m
+++ NewCalculator/LeopardButton.m
@@ -20,26 +20,45 @@
     return self;
 }
 
+- (void)setCommonProperties
+{
+    self.layer.cornerRadius = 20.0;
+    self.layer.masksToBounds = YES;
+}
+
+- (void)setUpNormalState
+{
+    [self setBackgroundImage:[UIImage imageNamed:@"leopardskin.jpg"] forState:UIControlStateNormal];
+    [self setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+}
+
+- (void)setBackgroundColor:(UIColor *)color forState:(UIControlState)state
+{
+    // Create a 1 by 1 pixel context
+    CGRect rect = CGRectMake(0, 0, 1, 1);
+    UIGraphicsBeginImageContextWithOptions(rect.size, NO, 0);
+    [color setFill];
+    UIRectFill(rect);
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    [self setBackgroundImage:image forState:UIControlStateHighlighted];
+    
+}
+
+- (void)setUpHighlightedState
+{
+    [self setBackgroundColor:[UIColor whiteColor] forState:UIControlStateHighlighted];
+    [self setTitleColor:[UIColor blackColor] forState:UIControlStateHighlighted];
+}
+
 - (id)initWithCoder:(NSCoder *)aDecoder
 {
     self = [super initWithCoder:aDecoder];
     if (self) {
-        [self setBackgroundImage:[UIImage imageNamed:@"leopardskin.jpg"] forState:UIControlStateNormal];
-        self.layer.cornerRadius = 20.0;
-        self.layer.masksToBounds = YES;
-        [self setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-        
-        // TODO refactor
-        CGRect rect = CGRectMake(0, 0, 1, 1);
-        // Create a 1 by 1 pixel context
-        UIGraphicsBeginImageContextWithOptions(rect.size, NO, 0);
-        [[UIColor whiteColor] setFill];
-        UIRectFill(rect);   // Fill it with your color
-        UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-        UIGraphicsEndImageContext();
-        
-        [self setBackgroundImage:image forState:UIControlStateHighlighted];
-        [self setTitleColor:[UIColor blackColor] forState:UIControlStateHighlighted];
+        [self setCommonProperties];
+        [self setUpNormalState];
+        [self setUpHighlightedState];
     }
     
     return self;
