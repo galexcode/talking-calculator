08763e1cc98dc18e2753df6a9b6eb4f5b76fc783
Olof Hellquist <olofhellquist@gmail.com>
2013-08-15 22:22:44 +0200

    Increased performance when transitioning between views by using core graphics instead of layers.

--- NewCalculator/LeopardButton.m
+++ NewCalculator/LeopardButton.m
@@ -9,8 +9,11 @@
 #import "LeopardButton.h"
 #import <QuartzCore/QuartzCore.h>
 
-@implementation LeopardButton
+@interface LeopardButton ()
+@property (nonatomic) BOOL isInitialised;
+@end
 
+@implementation LeopardButton
 
 - (id)initWithFrame:(CGRect)frame
 {
@@ -20,57 +23,88 @@
     return self;
 }
 
-- (void)setCommonProperties
-{
-    self.layer.cornerRadius = 20.0;
-    self.layer.masksToBounds = YES;
-}
 
-- (void)setUpNormalState
+- (void)setUpTitleForStates
 {
-    [self setBackgroundImage:[UIImage imageNamed:@"leopardskin.jpg"] forState:UIControlStateNormal];
     [self setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-}
-
-- (void)setBackgroundColor:(UIColor *)color forState:(UIControlState)state
-{
-    // Create a 1 by 1 pixel context
-    CGRect rect = CGRectMake(0, 0, 1, 1);
-    UIGraphicsBeginImageContextWithOptions(rect.size, NO, 0);
-    [color setFill];
-    UIRectFill(rect);
-    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    
-    [self setBackgroundImage:image forState:UIControlStateHighlighted];
-    
-}
-
-- (void)setUpHighlightedState
-{
-    [self setBackgroundColor:[UIColor whiteColor] forState:UIControlStateHighlighted];
     [self setTitleColor:[UIColor blackColor] forState:UIControlStateHighlighted];
 }
 
 - (id)initWithCoder:(NSCoder *)aDecoder
 {
     self = [super initWithCoder:aDecoder];
+    
     if (self) {
-        [self setCommonProperties];
-        [self setUpNormalState];
-        [self setUpHighlightedState];
+        self.isInitialised = NO;
     }
     
     return self;
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
+- (CGContextRef)getMaskedContext:(CGRect)rect witRadius:(int)radius
+{
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSaveGState(context);
+    CGPathRef clippath = [UIBezierPath bezierPathWithRoundedRect:rect cornerRadius:radius].CGPath;
+    CGContextAddPath(context, clippath);
+    CGContextClip(context);
+    
+    return context;
+}
+
+- (UIImage *)restoreContextAndGetImage:(CGContextRef)context
+{
+    CGImageRef imageRef = CGBitmapContextCreateImage(context);
+    UIImage* image = [UIImage imageWithCGImage:imageRef];
+    
+    CGImageRelease(imageRef);
+    CGContextRestoreGState(context);
+    
+    return image;
+}
+
+- (void)setBackground:(CGRect)rect forHighlightedStateUsingRadius:(int)radius
+{
+    const CGFloat *color = CGColorGetComponents([[UIColor whiteColor] CGColor]);
+    CGContextRef context = [self getMaskedContext:rect witRadius:radius];
+    
+    // Fill the context with white color
+    CGContextSetRGBFillColor(context, color[0], color[0], color[0], 1.f);
+    CGContextFillRect(context, rect);
+    CGContextSaveGState(context);
+    
+    UIImage *image = [self restoreContextAndGetImage:context];
+    [self setBackgroundImage:image forState:UIControlStateHighlighted];
+}
+
+- (void)setBackground:(CGRect)rect forNormalStateUsingRadius:(int)radius
+{
+    CGContextRef context = [self getMaskedContext:rect witRadius:radius];
+    
+    UIImage *originalImage = [UIImage imageNamed:@"leopardskin.jpg"];
+    [originalImage drawInRect:rect];
+    
+    UIImage *maskedImage = [self restoreContextAndGetImage:context];
+    [self setBackgroundImage:maskedImage forState:UIControlStateNormal];
+}
+
+- (void)setUpBackgroundForStates:(CGRect)rect
+{
+    int radius = 20;
+    [self setBackground:rect forNormalStateUsingRadius:radius];
+    [self setBackground:rect forHighlightedStateUsingRadius:radius];
+}
+
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect
 {
-    // Drawing code
+    [super drawRect:rect];
+    
+    if (!self.isInitialised) {
+        [self setUpBackgroundForStates:rect];
+        [self setUpTitleForStates];
+        self.isInitialised = YES;
+    }
 }
-*/
 
 @end
